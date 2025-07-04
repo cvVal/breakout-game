3e2bc6c28908374cf1934ba9eed0aaded0ff7451
cvVal <cvvaldez_00@hotmail.com>
2025-07-04 14:36:06 -0400

    add health to the blocks

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] int health = 1;
+    SpriteRenderer spriteRenderer;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -19,11 +22,28 @@ public class Block : MonoBehaviour
         // Check if the collided object is the ball
         if (collision.gameObject.CompareTag("Ball"))
         {
-            // Destroy this block
-            Destroy(gameObject);
+            // Decrease the health of the block
+            health--;
 
-            // Optionally, you can add a delay before destroying the block by passing the unit of time in seconds
-            // Destroy(gameObject, 2f);
+            switch (health)
+            {
+                case 3:
+                    spriteRenderer.color = Color.red; // Full health
+                    break;
+                case 2:
+                    spriteRenderer.color = Color.green; // Half health
+                    break;
+                case 1:
+                    spriteRenderer.color = Color.yellow; // Low health
+                    break;
+                default:
+                    Destroy(gameObject); // No health left
+                    /* 
+                    Optionally, you can add a delay before destroying the block by passing the unit of time in seconds
+                    Destroy(gameObject, 2f);
+                    */
+                    break;
+            }
         }
     }
 }
